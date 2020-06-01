a7f90dde4789a208d180c785b70a42b93e3a6f70
Kaan <kaanyrdkl@gmail.com>
2020-06-01 22:06:40 +0300

    1.6.2020

---
+++ MyNote.Desktop/PostTokenInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNote.Desktop
+{
+    public class PostTokenInput
+    {
+        public string grant_type { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
